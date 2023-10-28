6436be2a23efab6b51a190b8a030f7a9f8a113a5
ns832 <ns832@cam.ac.uk>
2023-10-28 12:52:12 +0100

    Evaluation script working, plots P-R curve.

--- eval_BERT.py
+++ eval_BERT.py
@@ -11,6 +11,7 @@ from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import PrecisionRecallDisplay
 from keras_preprocessing.sequence import pad_sequences
 from transformers import BertTokenizer, BertForSequenceClassification
+import matplotlib.pyplot as plt
 
 
 parser = argparse.ArgumentParser(description='Get all command line arguments.')
@@ -44,24 +45,25 @@ def load_trained_model(args, device):
     tokenizer = BertTokenizer.from_pretrained("prajjwal1/bert-small", do_lower_case=True)
     return model, tokenizer
 
-def load_dataset(tokenizer, device, args):
+def load_dataset(tokenizer, args):
     prompt_ids, resp_ids, att_mask_prompts, att_mask_resps = [], [], [], []
-    targets = np.loadtxt(args.labels_path)
+    targets = np.loadtxt(args.labels_path, dtype=int)
     targets = torch.tensor(targets)
     
     with open(args.prompts_path) as f0, open(args.responses_path) as f1:
         prompts, responses = f0.readlines(), f1.readlines()
-        prompts = [x.strip().lower() for x in prompts[:10]]
-        responses = [x.strip().lower() for x in responses[:10]]
+        prompts = [x.strip().lower() for x in prompts]
+        responses = [x.strip().lower() for x in responses]
         
-        max_prompt_length = max([len(sentence) for sentence in prompts])
-        max_resp_length = max([len(sentence) for sentence in responses])
-        max_length = max(max_prompt_length, max_resp_length)
+        max_prompt_length = 256
+        max_resp_length = 256
+        # max_prompt_length = max([len(sentence) for sentence in prompts])
+        # max_resp_length = max([len(sentence) for sentence in responses])
         
         for (prompt, response) in zip(prompts, responses):
-            prompt_id, att_mask = encode_data(tokenizer, prompt, max_length)
+            prompt_id, att_mask = encode_data(tokenizer, prompt, max_prompt_length)
             prompt_ids.append(prompt_id), att_mask_prompts.append(att_mask)
-            resp_id, att_mask = encode_data(tokenizer, response, max_length)
+            resp_id, att_mask = encode_data(tokenizer, response, max_resp_length)
             resp_ids.append(resp_id), att_mask_resps.append(att_mask)
     
     print("Dataset Loaded")
@@ -77,38 +79,35 @@ def encode_data(tokenizer, inputs, MAX_LEN):
     return input_ids, attention_mask
 
 def eval_model(args, model, device, prompt_ids, resp_ids, att_mask_prompts, att_mask_resps, targets):
-
-    eval_dataset = TensorDataset(prompt_ids, att_mask_prompts, resp_ids, att_mask_resps, targets[:10])
+    prompt_ids = prompt_ids.squeeze(1)
+    att_mask_prompts = att_mask_prompts.squeeze(1)
+    resp_ids = resp_ids.squeeze(1)
+    att_mask_resps = att_mask_resps.squeeze(1)
+    eval_dataset = TensorDataset(prompt_ids, att_mask_prompts, resp_ids, att_mask_resps, targets)
     eval_dataloader = DataLoader(eval_dataset, batch_size = args.batch_size, shuffle = False)
 
     model.eval()    
     y_pred_all = []
     print("Started evaluation")
     
-    for prompt_id, att_mask_prompt, resp_id, att_mask_resp, target in zip(prompt_ids, att_mask_prompts, resp_ids, att_mask_resps, targets):
+    #enumerate(train_dataloader)??
+    for prompt_id, att_mask_prompt, resp_id, att_mask_resp, target in eval_dataloader:
         pr_resp, pr_resp_msk = torch.cat((prompt_id, resp_id), 1), torch.cat((att_mask_prompt, att_mask_resp), 1)  
         pr_resp, pr_resp_msk = send_to_device(device, (pr_resp)), send_to_device(device, pr_resp_msk)        
         prompt_id, att_mask_prompt, resp_id, att_mask_resp, target = send_to_device(device, prompt_id), send_to_device(device, att_mask_prompt), send_to_device(device, resp_id) , send_to_device(device, att_mask_resp), send_to_device(device, target) 
         
-        print(pr_resp.size())
-        print(pr_resp_msk.size())
-        print(target.size())
-        
         with torch.no_grad():
-            outputs = model(pr_resp, attention_mask=pr_resp_msk, labels=target)
-        # logits = (outputs[1]).to('cpu')
+            outputs = model(pr_resp.squeeze(1), token_type_ids=None, attention_mask=pr_resp_msk.squeeze(1), labels=target)
         logits = outputs[1]
-        logits = logits.detach().to(device)
+        logits = logits.cpu()
+        logits = logits.detach().numpy()
         logits = np.squeeze(logits[:, 1])
         logits = logits.tolist()
-        print(logits)
-        break
-        y_pred_all += logits
+        y_pred_all += logits  
         
     y_pred_all = np.array(y_pred_all)
-    # Save the predicted values so that they can be used for ensembling
-    np.savetxt(args.predictions_save_path, y_pred_all)
-    print("Predictions saved")
+    # np.savetxt(args.predictions_save_path, y_pred_all)
+    # print("Predictions saved")
     calculate_metrics(targets, y_pred_all)
     return
 
@@ -117,9 +116,23 @@ def calculate_metrics(targets, y_pred_all):
     targets = 1.-targets
     y_pred = 1.-y_pred_all
     targets, y_pred = torch.tensor(targets, device = 'cpu'), torch.tensor(y_pred, device = 'cpu')
+    print(targets.size(), y_pred.size())
     precision, recall, _ = precision_recall_curve(targets, y_pred)
     print("Precision:", precision)
     print("Recall:", recall)
+    
+    #create precision recall curve
+    fig, ax = plt.subplots()
+    ax.plot(recall, precision, color='purple')
+
+    #add axis labels to plot
+    ax.set_title('Precision-Recall Curve')
+    ax.set_ylabel('Precision')
+    ax.set_xlabel('Recall')
+
+    #display plot
+    fig.show()
+    fig.savefig('/scratches/dialfs/alta/relevance/ns832/results' + '/eval' +  '_plot.jpg', bbox_inches='tight', dpi=150)
     f_score = np.amax( (1.+0.5**2) * ( (precision * recall) / (0.5**2 * precision + recall) ) )
     print("F0.5 score is:", f_score)
 
@@ -127,7 +140,7 @@ def calculate_metrics(targets, y_pred_all):
 def main(args):
     device = get_default_device()
     model, tokenizer = load_trained_model(args, device)
-    prompt_ids, resp_ids, att_mask_prompts, att_mask_resps, targets = load_dataset(tokenizer, device, args)
+    prompt_ids, resp_ids, att_mask_prompts, att_mask_resps, targets = load_dataset(tokenizer, args)
     eval_model(args, model, device, prompt_ids, resp_ids, att_mask_prompts, att_mask_resps, targets)
 
 
