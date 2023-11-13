d1244f02a761f4908d248b465262a2e31500ac50
ns832 <ns832@cam.ac.uk>
2023-11-13 21:26:55 +0000

    Code added to the visual+textual attempt to try and match image to prompt. Problems with this so commiting as a checkpoint incase changes need to be reverted

--- train_visual.py
+++ train_visual.py
@@ -8,16 +8,22 @@ from torchvision import transforms
 from torchvision.datasets import ImageFolder
 import os
 from PIL import Image
-import eval_BERT
+from transformers import BertTokenizer
+import eval_BERT, train_BERT
 
 parser = argparse.ArgumentParser(description='Get all command line arguments.')
 parser.add_argument('--images_path', type=str, help='Load path of image training data')
 parser.add_argument('--batch_size', type=int, default=100, help='Specify the test batch size')
 parser.add_argument('--prompts_path', type=str, help='Load path to test prompts as text')
-parser.add_argument('--resps_path', type=str, help='Load path to test responses as text')
-parser.add_argument('--labels_path', type=str, help='Load path to labels')
+parser.add_argument('--responses_path', type=str, help='Load path of answer training data')
+parser.add_argument('--prompt_ids_path', type=str, help='Load path of prompt ids')
+parser.add_argument('--topics_path', type=str, help='Load path of question training data')
+parser.add_argument('--topic_dist_path', type=str, help='Load path of question training data')
+parser.add_argument('--image_ids_to_prompts_path', type=str, help='Load path of question training data')
 parser.add_argument('--model_path', type=str, help='Load path to trained model')
 parser.add_argument('--predictions_save_path', type=str, help="Where to save predicted values")
+parser.add_argument('--seed', type=int, default=1, help='Specify the global random seed')
+
 
 
 # Global Variables
@@ -25,25 +31,12 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(device)
 args = parser.parse_args()
 
-class arguments:
-    def __init__(self, batch_size, prompts_path, resps_path, labels_path, model_path, predictions_save_path):
-        self.batch_size = batch_size
-        self.prompts_path = prompts_path
-        self.resps_path = resps_path
-        self.labels_path = labels_path
-        self.model_path = model_path
-        self.predictions_save_path = predictions_save_path
 
 def format_time(elapsed):
     elapsed_rounded = int(round((elapsed)))
     return str(datetime.timedelta(seconds=elapsed_rounded))
 
-def create_dataset(train_dataset_images):
-    train_dataloader_images = DataLoader(train_dataset_images, batch_size=args.batch_size)
-    return train_dataloader_images
-
-
-def load_images():
+def load_images(id_list):
     image_size = (465, 770)
     preprocess = transforms.Compose([
         transforms.Resize(image_size),
@@ -55,7 +48,6 @@ def load_images():
     ])
     images = []
     for file in os.listdir(args.images_path):
-        # Folder contains not just .png but also .htm
         if file.endswith(".png"): 
             image_path = os.path.join(args.images_path, file)
             image = Image.open(image_path).convert("RGB")
@@ -82,24 +74,71 @@ def encode_images(images, image_processor):
     pixel_values = pixel_values.to(device)
     return inputs_list, pixel_values
 
+def create_id_dictionary(args):
+    id_dict = dict()
+    with open(args.image_ids_to_prompts_path) as f:
+        image_ids_to_prompts = f.readlines()
+        for line in image_ids_to_prompts:
+            image_id = line.split()[0]
+            prompt =  line.replace(image_id, "")
+            prompt = prompt.replace("\t", "")
+            prompt = prompt.replace("\n", "")
+            if "/" in image_id: # Extract first id associated with the prompt
+                image_id = image_id.split("/")[0]
+            id_dict[prompt] = image_id
+    id_list = []
+    prompts = open(args.prompts_path).readlines()
+    for prompt in prompts:
+        prompt = prompt.replace("\n", "")
+        if prompt in id_dict.keys():
+            id_list.append(id_dict[prompt])
+        else:
+            print(prompt)
+    raise
+    return id_dict
+
+def create_dataset(encoded_prompts, encoded_responses, VT_outputs, VT_attention_mask, prompt_attention_masks, response_attention_masks, targets):
+    encoded_prompts = encoded_prompts.squeeze(1)
+    encoded_responses = encoded_responses.squeeze(1)
+    VT_outputs = VT_outputs.squeeze(1)
+    VT_attention_mask = VT_attention_mask.squeeze(1)
+    prompt_attention_masks = prompt_attention_masks.squeeze(1)
+    response_attention_masks = response_attention_masks.squeeze(1)
+    
+    print(encoded_prompts.shape), print(encoded_responses.shape), print(VT_outputs.shape)
+    input = torch.cat((encoded_prompts.cpu(), encoded_responses.cpu(), VT_outputs.cpu()),1)
+    input_mask = torch.cat((prompt_attention_masks, response_attention_masks, VT_attention_mask),1)
+    input = input.squeeze(1)
+    input_mask = input_mask.squeeze(1)
+    print(input.size(), input_mask.size(), targets.size())
+    
+    train_data = TensorDataset(input, input_mask, targets)
+    train_sampler = RandomSampler(train_data)
+    train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=args.batch_size)
+    return train_dataloader
+
 def main():
+    # Match the images to their prompts
+    id_dict = create_id_dictionary(args)
+    
+    # Obtain BERT outputs
+    bert_base_uncased = "prajjwal1/bert-small"
+    train_BERT.set_seed(args)
+    encoded_prompts, encoded_responses, prompt_attention_masks, response_attention_mask, targets, id_list = train_BERT.load_dataset(args, bert_base_uncased, id_dict)
+    
     # Obtain VT outputs (not used in training, only evaluation)
-    images = load_images()
+    images = load_images(id_list)
     model, image_processor = load_model()
     inputs_list, pixel_values = encode_images(images, image_processor)
     with torch.no_grad():
         VT_outputs = model(pixel_values) 
-        VT_outputs = VT_outputs.logits
-        print(VT_outputs)
-    # outputs are then concatenated with the outputs of the BERT system only during the evaluation
-    args_model = arguments(args.batch_size, args.prompts_path, args.resps_path, args.labels_path, args.model_path, args.predictions_save_path)
-    BERT_outputs = eval_BERT.main(args_model)
-    BERT_outputs = torch.tensor(BERT_outputs)
+        VT_outputs = VT_outputs.logits    
+    
+    # outputs are then concatenated together, with attention mask set to zero for the visual elements
+    VT_attention_mask = np.zeros_like(VT_outputs.cpu())
+    VT_attention_mask = torch.tensor(VT_attention_mask)
+    train_dataloader = create_dataset(encoded_prompts, encoded_responses, VT_outputs, VT_attention_mask, prompt_attention_masks, response_attention_mask, targets)
 
-    print(VT_outputs)
-    print(BERT_outputs)
-    concatenated_outputs = VT_outputs + BERT_outputs
-    return VT_outputs
 
 if __name__ == '__main__':
     main() 
