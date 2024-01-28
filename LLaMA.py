4ace49c3d7ea52e1e0a680365404e97a6cecc6a3
ns832 <ns832@cam.ac.uk>
2024-01-28 12:00:19 +0000

    Prompting for LLaMA now achieving On/Off responses.

--- LLaMA.py
+++ LLaMA.py
@@ -3,9 +3,10 @@ import torch
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler
 import numpy as np
 from sklearn.metrics import precision_recall_curve
-from transformers import LlamaForCausalLM, LlamaTokenizer
+from transformers import LlamaForCausalLM, LlamaTokenizer, AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline, TextStreamer
 import matplotlib.pyplot as plt
 import Section_D.preprocess_data as preprocess_data
+import train_BERT
 
 
 parser = argparse.ArgumentParser(description='Get all command line arguments.')
@@ -23,14 +24,53 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(device)
 args = parser.parse_args()
 
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+)
+
 
 def initialise_model():
     """
         Instantiates the tokeniser and model
     """
-    model_id =  "enoch/llama-7b-hf"
-    tokenizer = LlamaTokenizer.from_pretrained(model_id)
-    model = LlamaForCausalLM.from_pretrained(model_id, load_in_8bit=True,)
+    model_name = "mistralai/Mistral-7B-v0.1"
+    tokenizer = AutoTokenizer.from_pretrained(model_name,add_special_tokens=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            load_in_4bit=True,
+            quantization_config=bnb_config,
+            torch_dtype=torch.bfloat16,
+            device_map="auto",
+            trust_remote_code=True,
+    )  
+    # model.to(device)
+    
+    # pipe = pipeline(
+    #     "text-generation", 
+    #     model=model, 
+    #     tokenizer = tokenizer, 
+    #     torch_dtype=torch.bfloat16, 
+    #     device_map="auto"
+    # ) 
+    # # <s>[INST] Instruction [/INST] Model answer</s>[INST] Follow-up instruction [/INST]
+    # prompt = """<s>[INST] Respond to the prompts with only 'On' or 'Off' depending on if the answer is relevant to the question asked. For instance:
+    #                     [Question] this chart shows the number of positive and negative responses given in a survey concerning customer satisfaction with a hotel look at the information and talk about the results of the customer survey.
+    #                     [Answer] this charts present results of survey in hotel most negative response was given about the value for money it was about about nine hundred person and the best results get response for question about attitude of staff it was at about nine hundred and half percent and at about half of respondents talk that parking is positive and at about. 
+    #                     Would be converted to:[/INST] On </s>
+    #                     [INST] [Question] these charts show the average monthly sales of business today magazine between two thousand five and two thousand eight together with the percentage of purchasers in each age group look at the information and talk about the magazine's sales and purchasers between two thousand five and two thousand eight. 
+    #                     [Answer] as you can see from the chart the sales of business today magazine has been increasing from two thousand five to two thousand six and maintain steady in two thousand seven but due to the increase in the magazines the sales has considerably decreased in two thousand eight plus there is one notice differences the age of the subscribers the people and the age range of zero to twenty has increased reading out the people in the age range of thirty five to forty has been reduced this could probably mean that they have cited for different means of reading the news tv more likely or a papers as the other than the physical business today magazines one thing we have to notices and people the age range of forty to forty five has been the most consistent of all the age groups in the years two thousand five to two thousand eight. [/INST]"""
+
+    # sequences = pipe(
+    #     prompt,
+    #     do_sample=False,
+    #     max_new_tokens=10, 
+    #     num_return_sequences=1,
+    # )
+    # print(sequences[0]['generated_text'])
+    # raise
     return model, tokenizer
 
 
@@ -42,36 +82,37 @@ def get_targets(args, topics, text_data):
     """
     # If no targets, assume all responses are on-topic
     if args.labels_path == None:
+        for data in text_data:
+            data.target = 1
         text_data = preprocess_data.permute_data(text_data[:1000], topics, args)
-        targets = [1] * int(len(text_data) / 2) + [0] * int(len(text_data) / 2)
     else:
         targets = np.loadtxt(args.labels_path, dtype=int)
-        targets = targets[:len(text_data)]
+        for data, target in zip(text_data, targets):
+            data.target = target
         
-    # Print prevelance of off-topic responses
-    off_targets = [x for (x, y) in zip(text_data, targets) if y == 0]
-    print("Dataset size: ", len(text_data) , "Proportions: ", len(off_targets) / len(targets))
-    assert (len(targets) == len(text_data))
-
-    # Shuffle the text_data and targets together
-    temp = list(zip(text_data, targets))
-    np.random.shuffle(temp)
-    text_data, targets = zip(*temp)
-    targets = torch.tensor(targets)
+    np.random.shuffle(text_data)
+    text_data = text_data[:1000]
+    off_targets = [x for x in text_data if x.target == 0]
+    print("Dataset size: ", len(text_data) , "Proportions: ", len(off_targets) / len(text_data))
     
-    return text_data, targets
+    return text_data
 
 
 def create_prompts(text_data):
     """
         Creates prompts to feed into the LLM with the prompt and response pairs.
-    """
-    LLaMA_prompt = 'Given this question and answer pair, return a single-word response of either ”On” or ”Off” depending on if it is on-topic or off-topic. '
+    """    
     LLaMA_prompt_list = []
     prompts = [x.prompt.strip().lower().replace("</s>", "") for x in text_data]
     responses = [x.response.strip().lower().replace("</s>", "") for x in text_data]
     for prompt, response in zip(prompts, responses):
-        LLaMA_prompt_list.append(LLaMA_prompt + 'Question: ' + prompt + '. Answer: ' + response)
+        LLaMA_prompt = """<s>[INST] Respond to the prompts with only 'On' or 'Off' depending on if the answer is relevant to the question asked. For instance:
+                        [Question] this chart shows the number of positive and negative responses given in a survey concerning customer satisfaction with a hotel look at the information and talk about the results of the customer survey.
+                        [Answer] this charts present results of survey in hotel most negative response was given about the value for money it was about about nine hundred person and the best results get response for question about attitude of staff it was at about nine hundred and half percent and at about half of respondents talk that parking is positive and at about. 
+                        Would be converted to:[/INST] On </s>
+                        [INST] [Question] """ + prompt + """. 
+                        [Answer] """  + response + ". [/INST]"
+        LLaMA_prompt_list.append(LLaMA_prompt)
     print("Random prompt: ", LLaMA_prompt_list[np.random.randint(0, 1000)])
     return LLaMA_prompt_list
 
@@ -81,45 +122,45 @@ def create_dataloader(tokenizer, prompt_list, device):
         Creates a dataloader with the encoded input ids and attention masks.
         Pad token is set to the eos token to allow for all encodings to be equal length.
     """
-    tokenizer.pad_token = tokenizer.eos_token
-    encoding = tokenizer(prompt_list, return_tensors="pt", truncation=True, padding="longest", max_length = 256).to(device)
-    input_ids = torch.tensor(encoding.input_ids).to(device)
-    attention_mask = torch.tensor(encoding.attention_mask).to(device)
+    attention_mask, encoding = [], []
+    for prompt in prompt_list:
+        encoded_prompt = tokenizer(prompt, padding='max_length', truncation=True, max_length=512, add_special_tokens=False)
+        encoding.append(encoded_prompt["input_ids"])
+        attention_mask.append(encoded_prompt["attention_mask"])
+    input_ids = torch.tensor(encoding).squeeze().to(device)
+    attention_mask = torch.tensor(attention_mask).squeeze().to(device)
+    print(input_ids.shape)
     
     train_data = TensorDataset(input_ids, attention_mask)
     train_sampler = RandomSampler(train_data)
-    train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=10)
+    train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=1)
     
     return train_dataloader
 
 
-def model_generate(model, tokenizer, device, train_dataloader):
+def model_generate(model, tokenizer, device, train_dataloader, text_data):
     """
         Iterated through the data in batches, calling the LLM for each batch to extract the outputs.
         From there the outputs are passed through get_probabilities() to extract the probability of 
         an 'on topic' response.
     """
     on_probabilities = []
+    streamer = TextStreamer(tokenizer, skip_prompt=False, skip_special_tokens=True)
     for step, batch in enumerate(train_dataloader):
-        print(step, " of ", len(train_dataloader))
+        print(step, " of ", len(train_dataloader), " Target: ", text_data[step].target)
         with torch.no_grad():
             b_input_ids = batch[0].to(device)
             b_att_msks = batch[1].to(device)
             output = model.generate(input_ids=b_input_ids, 
                                     attention_mask=b_att_msks, 
+                                    pad_token_id=tokenizer.eos_token_id,
+                                    do_sample = False, # temperature = 0
                                     return_dict_in_generate=True,
                                     max_new_tokens=1,
+                                    streamer=streamer,
                                     output_scores=True, 
                                     use_cache=False)
-            generated_ids = output.sequences[0]  # Extract the generated sequence
-
-            # Filter out special tokens
-            filtered_ids = [token_id.item() for token_id in generated_ids if token_id not in tokenizer.all_special_ids]
-
-            # Convert numeric values to words using the tokenizer
-            decoded_sequence = tokenizer.decode(filtered_ids, skip_special_tokens=True)
-            print(decoded_sequence)
-            raise
+            
             scores = (output.scores)[0].cpu().numpy()
             for score in scores:
                 logits = np.array(score)
@@ -136,8 +177,8 @@ def get_probabilities(logits, tokenizer):
     on_token = tokenizer("On").input_ids
     off_token = tokenizer("Off").input_ids
     
-    on_logits = logits[on_token[1]]
-    off_logits = logits[off_token[1]]
+    on_logits = logits[on_token[1]] 
+    off_logits = logits[off_token[1]] 
     score = np.exp(on_logits) / (np.exp(on_logits) + np.exp(off_logits))
     print("Logits:", on_logits, off_logits, "Score:", score)
     
@@ -227,15 +268,16 @@ def main(args):
     
     # Preprocess data
     text_data, __ , topics = preprocess_data.load_dataset(args, images = False)
-    text_data, targets = get_targets(args, topics, text_data)
+    text_data = get_targets(args, topics, text_data)
     LLaMA_prompt_list = create_prompts(text_data)
     
     # Run the evaluation
     train_dataloader = create_dataloader(tokenizer, LLaMA_prompt_list, device)
-    on_probabilities = model_generate(model, tokenizer, device, train_dataloader)   
+    on_probabilities = model_generate(model, tokenizer, device, train_dataloader, text_data)   
     prob_list = np.array(on_probabilities)
  
     # Create Precision-Recall Graph
+    targets = np.array([x.target for x in text_data])
     calculate_metrics(targets, prob_list)
     calculate_normalised_metrics(targets, prob_list)
 
